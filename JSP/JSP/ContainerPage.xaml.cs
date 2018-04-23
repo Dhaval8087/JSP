22c30135ab86cb4251c6cd50d79314a9f09dcdd7
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2018-04-23 18:35:52 +0530

    no message

---
+++ JSP/JSP/ContainerPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using JSP.ViewModels;
+using System.Windows;
+
+namespace JSP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class ContainerPage : Window
+    {
+        public ContainerPage()
+        {
+            InitializeComponent();
+            this.DataContext = new JSPViewModelBase();
+        }
+    }
+}
