14e2202d8df7374acb7535ee3641a0e49b511058
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2018-04-23 14:04:00 +0530

    Changes related to App.xaml page

---
+++ JSP/JSP/LoginPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using JSP.ViewModels;
+using System.Windows;
+
+namespace JSP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginPage : Window
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.DataContext = new LoginPageViewModel();
+        }
+    }
+}
---
+++ JSP/JSP/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace JSP.ViewModels
+{
+    internal class LoginPageViewModel : ViewModelBase
+    {
+        public LoginPageViewModel()
+        {
+        }
+    }
+}
