e7ccf2a0c42d21d101337fc24def760faa2f9ab5
ZEROCHAOS\dpatel2 <dpatel2@zerochaos.com>
2018-04-23 18:53:24 +0530

    Changes related packages

---
+++ JSP/JSP/ViewModels/JSPViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using GalaSoft.MvvmLight;
+using JSP.Views;
+using System.Windows.Controls;
+
+namespace JSP.ViewModels
+{
+    public  class JSPViewModelBase : ViewModelBase
+    {
+        public JSPViewModelBase()
+        {
+            Content.Content = new LoginPage();
+
+        }
+        private bool _IsBusy;
+
+        public bool IsBusy
+        {
+            get { return _IsBusy; }
+            set { _IsBusy = value; RaisePropertyChanged(); }
+        }
+
+        private ContentControl _Content;
+
+        public ContentControl Content
+        {
+            get { return _Content ?? (_Content = new ContentControl()); }
+            set { _Content = value; RaisePropertyChanged(); }
+        }
+
+
+
+    }
+}
